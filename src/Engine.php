b49b9180e03c1a94eca44ae29a5de3175ca14d6b
Maestro <otabekavazov2010@gmail.com>
2026-04-11 12:49:23 +0500

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -1,1 +1,34 @@
+<?php
 
+namespace Engine;
+
+class Engine extends RustEngine
+{
+    public function __construct()
+    {
+        $this->loadExtension();
+    }
+
+    private function loadExtension(): void
+    {
+        if (!extension_loaded("ssalom")) {
+
+            $ext = __DIR__ . "/../ext/libssalom.so";
+
+            if (!file_exists($ext)) {
+                throw new \Exception("❌ Extension file not found: $ext");
+            }
+
+            if (function_exists("dl")) {
+                dl($ext);
+            } else {
+                throw new \Exception(
+                    "❌ extension yuklanmagan!\n" .
+                    "👉 php.ini ga qo‘sh: extension=libssalom.so"
+                );
+            }
+        }
+    }
+
+
+}
